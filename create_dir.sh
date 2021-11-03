a42316b90ebc9eca60ae19eb2f2bb8fcde41d337
MingzheHu <58562242+MingzheHu-Duke@users.noreply.github.com>
2021-11-03 07:35:46 -0400

    Create create_dir.sh

---
+++ create_dir.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir 00ref \
+
+mkdir 01raw_data \
+
+mkdir 02quality_control \
+
+mkdir 03STAR_genome \
+
+mkdir 04align_out \
+
+mkdir 04align_out_Sam \
+
+mkdir 05RSEM \
+
+mkdir 05RESEM_Out \
+
+mkdir 06kallisto \
+
+mkdir 06kallisto_Out \
+
+mkdir 07htseq \
+
+mkdir 08feature_count \
+
+mkdir 09deseq_out
